2970f5f8603422250e486a418970615fddaf0513
EmmanuelJulio <emajulio.ej@gmail.com>
2022-01-21 23:48:43 -0300

    Agregado refactory

---
+++ Backend/Acceso a Datos/Interfaces/IServiceMascota.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinariaBackend.Dominio;
+
+namespace Backend.Acceso_a_Datos.Interfaces
+{
+    public interface IServiceMascota
+    {
+        public Mascota OptenerMascotaPorIdYNombre(int id, string nombre);
+        public Mascota OptenerMascotaPorId(int id);
+
+        public void AgregarMascota(Mascota mascota);
+        public void BorrarMascota(Mascota mascota);
+        public void ActualizarMascota(Mascota mascota);
+
+    }
+}
